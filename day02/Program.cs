4d9e3da678dbe55e5d7665d78605ea2ba82419f1
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2024-12-02 13:37:06 +0100

    day02 in C#

---
+++ day02/Program.cs
@@ -0,0 +1,22 @@
+﻿var reports = File.ReadLines("input.txt").Select(reportString => reportString.Split().Select(long.Parse).ToArray());
+
+Console.WriteLine("C#\n" + Environment.GetEnvironmentVariable("part") switch {
+    "part1" => reports.Select(ReportIsSafe).Count(isSafe => isSafe),
+    "part2" => reports.Select(
+        levels => levels.Select((_, skipIndex) => ReportIsSafe([..levels[..skipIndex], ..levels[(skipIndex + 1)..]])).Any(isSafe => isSafe)
+    ).Count(isSafe => isSafe),
+    _ => "Please run with environment variable part=part1 or part=part2"
+});
+
+static bool ReportIsSafe(long[] reportLevels) {
+    var diffGroups = Enumerable.Range(0, reportLevels.Length - 1).Select(i => LevelDiff(reportLevels[i], reportLevels[i + 1])).GroupBy(diff => diff).ToArray();
+    return diffGroups.Length == 1 && diffGroups[0].Key != Diff.UnsafeDiff;
+}
+
+static Diff LevelDiff(long a, long b) => (b - a) switch {
+    >= 1 and <= 3 => Diff.SafeIncreaseDiff,
+    <= -1 and >= -3 => Diff.SafeDecreaseDiff,
+    _ => Diff.UnsafeDiff
+};
+
+enum Diff { UnsafeDiff, SafeIncreaseDiff, SafeDecreaseDiff }
