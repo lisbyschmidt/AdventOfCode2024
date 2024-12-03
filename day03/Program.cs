87bddd1c5a0c1fcbda3f56c643c4b74ae25f6c87
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2024-12-03 14:41:23 +0100

    day03 in C#

---
+++ day03/Program.cs
@@ -0,0 +1,13 @@
+﻿var memory = File.ReadAllText("input.txt");
+
+Console.WriteLine("C#\n" + Environment.GetEnvironmentVariable("part") switch {
+    "part1" => SumOfMultiplications(memory),
+    "part2" => EnabledMultiplications(memory).Sum(SumOfMultiplications),
+    _ => "Please run with environment variable part=part1 or part=part2"
+});
+
+static long SumOfMultiplications(string memory) =>
+    Regex.Matches(memory, @"mul\((\d{1,3}),(\d{1,3})\)").Select(x => long.Parse(x.Groups[1].Value) * long.Parse(x.Groups[2].Value)).Sum();
+
+static IEnumerable<string> EnabledMultiplications(string memory) =>
+    Regex.Matches($"do(){memory}", @"(do\(\))(.*?)(don't\(\)|$)", RegexOptions.Singleline).Select(x => x.Groups[2].Value);
